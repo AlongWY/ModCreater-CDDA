a5a6f69b2492bb70c9505f4f5e5fc1599e1cd623
alongwy <15939423778@163.com>
2018-01-28 20:10:16 +0800

    [skip ci]添加多语言支持

---
+++ lang/lang_zh.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/Window.ui" line="14"/>
+        <location filename="../build/src/ui_Window.h" line="123"/>
+        <source>MainWindow</source>
+        <translation>Mod 编辑器</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="31"/>
+        <location filename="../build/src/ui_Window.h" line="125"/>
+        <source>MODS List</source>
+        <translation>Mod 列表</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="60"/>
+        <location filename="../build/src/ui_Window.h" line="126"/>
+        <source>ModInfo</source>
+        <translation>Mod 信息</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="65"/>
+        <location filename="../build/src/ui_Window.h" line="127"/>
+        <source>Bionics</source>
+        <translatorcomment>CBM那些东西吧</translatorcomment>
+        <translation>仿生部件</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="70"/>
+        <location filename="../build/src/ui_Window.h" line="128"/>
+        <source>Material</source>
+        <translation>材料</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="82"/>
+        <location filename="../build/src/ui_Window.h" line="129"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../ui/Window.ui" line="89"/>
+        <location filename="../ui/Window.ui" line="99"/>
+        <location filename="../build/src/ui_Window.h" line="124"/>
+        <location filename="../build/src/ui_Window.h" line="130"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+</context>
+<context>
+    <name>cdda::MainWindow</name>
+    <message>
+        <location filename="../src/View/MainWindow.cpp" line="16"/>
+        <source>OnExit()</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../src/View/MainWindow.cpp" line="21"/>
+        <source>OnSave()</source>
+        <translation>保存</translation>
+    </message>
+</context>
+</TS>
